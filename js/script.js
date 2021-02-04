f78868ce37eda6ba07682b9dc0de31af814ea477
SebastianZabdyr <zabdyr6@wp.pl>
2021-02-04 19:21:51 +0100

    add computerMove for script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+  computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
