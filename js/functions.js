97903e0e41cfdda31d42af904775cbbfb0f5fd19
SebastianZabdyr <zabdyr6@wp.pl>
2021-02-05 18:42:46 +0100

    Add main fuction & fix code for functions

--- js/functions.js
+++ js/functions.js
@@ -1,46 +1,48 @@
-function printMessage(msg) {
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages() {
-	document.getElementById('messages').innerHTML = '';
-}
-
-function getMoveName(argMoveId) {
- console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-  return 'kamień';
-  } else if (argMoveId == 2) {
-  return 'papier';
-  } else if (argMoveId == 3) {
-  return 'nożyce';
-  } else {
-	printMessage ('zła liczba')
+function playGame(){
+	function printMessage(msg) {
+		let div = document.createElement('div');
+		div.innerHTML = msg;
+		document.getElementById('messages').appendChild(div);
 	}
-}
 
-function displayResult(argComputerMove, argPlayerMove) {
-	console.log('wywołano funkcję displayResult z argumentem: ' + argComputerMove + argPlayerMove);
-	printMessage('Komputer zagrał ' + argComputerMove + ', Użytkownik zagrał ' + argPlayerMove);
-	if ( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-	printMessage('Ty wygrywasz!');
-	} else if ( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-	printMessage('Ty wygrywasz!');
-	} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-	printMessage('Ty wygrywasz!');
-	} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-	printMessage('Ty wygrywasz!');
-	} else if ( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-	printMessage('Ty wygrywasz!');
-	} else if ( argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
-	printMessage('Remis!');
-	} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
-	printMessage('Remis!');
-	} else if ( argComputerMove == 'papier' && argPlayerMove == 'papier') {
-	printMessage('Remis!');
-	} else {
-	printMessage ('przegrywasz')
+	function clearMessages() {
+		document.getElementById('messages').innerHTML = '';
+	}
+
+	function getMoveName(argMoveId) {
+ 		console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  	if (argMoveId == 1) {
+  			return 'kamień';
+  	} else if (argMoveId == 2) {
+  			return 'papier';
+  	} else if (argMoveId == 3) {
+  			return 'nożyce';
+  	} else {
+				printMessage ('zła liczba')
+		}
+	}
+
+	function displayResult(argComputerMove, argPlayerMove) {
+		console.log('wywołano funkcję displayResult z argumentem: ' + argComputerMove + argPlayerMove);
+				printMessage('Komputer zagrał ' + argComputerMove + ', Użytkownik zagrał ' + argPlayerMove);
+		if ( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+				printMessage('Ty wygrywasz!');
+		} else if ( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+				printMessage('Ty wygrywasz!');
+		} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+				printMessage('Ty wygrywasz!');
+		} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+				printMessage('Ty wygrywasz!');
+		} else if ( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+				printMessage('Ty wygrywasz!');
+		} else if ( argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+				printMessage('Remis!');
+		} else if ( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+				printMessage('Remis!');
+		} else if ( argComputerMove == 'papier' && argPlayerMove == 'papier') {
+				printMessage('Remis!');
+		} else {
+				printMessage ('przegrywasz')
+		}
 	}
 }
